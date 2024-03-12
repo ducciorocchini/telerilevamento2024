e5e1d80a700b6f8869eb020030197da191656ecd
Duccio Rocchini <ducciorocchini@gmail.com>
2024-03-12 17:13:35 +0100

    Update and rename visualisation.R to 03_visualisation.R

--- Code/03_visualisation.R
+++ Code/03_visualisation.R
@@ -1,10 +1,5 @@
 # RS data
 
-library(devtools) # packages in R are also called libraries
-
-# install the imageRy package from GitHub
-install_github("ducciorocchini/imageRy")  # from devtools
-
 library(imageRy)
 library(terra)
 # in case you have not terra

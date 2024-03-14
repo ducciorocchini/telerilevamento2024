11b5c1be66afb005ab7694c4922ef3f6f35f66a3
Duccio Rocchini <ducciorocchini@gmail.com>
2024-03-14 17:09:00 +0100

    Update 03_visualisation.R

--- Code/03_visualisation.R
+++ Code/03_visualisation.R
@@ -1,4 +1,5 @@
 # Satellite data visualisation in R by imageRy
+# https://sentinels.copernicus.eu/web/sentinel/user-guides/sentinel-2-msi/resolutions/spatial
 
 library(terra)
 library(imageRy)
@@ -7,4 +8,34 @@ library(imageRy)
 im.list()
 
 # Importing data
-mato <- im.import("matogrosso_ast_2006209_lrg.jpg")
+b2 <- im.import("sentinel.dolomites.b2.tif")
+
+# Plotting the data
+clg <- colorRampPalette(c("black", "grey", "light grey"))(3)
+plot(b2, col=clg)
+
+clg <- colorRampPalette(c("black", "grey", "light grey"))(100)
+plot(b2, col=clg)
+
+clcyan <- colorRampPalette(c("magenta", "cyan4", "cyan"))(3)
+plot(b2, col=clcyan)
+
+clcyan <- colorRampPalette(c("magenta", "cyan4", "cyan"))(100)
+plot(b2, col=clcyan)
+
+clch <- colorRampPalette(c("magenta", "cyan4", "cyan", "chartreuse"))(100)
+plot(b2, col=clch)
+
+# importing the additional bands
+b3 <- im.import("sentinel.dolomites.b3.tif")
+plot(b3, col=clch)
+
+b4 <- im.import("sentinel.dolomites.b4.tif")
+plot(b4, col=clch)
+
+b8 <- im.import("sentinel.dolomites.b8.tif")
+plot(b8, col=clch)
+
+
+
+

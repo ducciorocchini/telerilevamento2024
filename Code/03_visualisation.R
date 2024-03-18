b90b998add492b17c3f1cb180880db735312c649
Duccio Rocchini <ducciorocchini@gmail.com>
2024-03-18 10:35:43 +0100

    Update 03_visualisation.R

--- Code/03_visualisation.R
+++ Code/03_visualisation.R
@@ -38,6 +38,12 @@ plot(b8, col=clch)
 
 # multiframe
 par(mfrow=c(2,2))
+plot(b2)
+plot(b3)
+plot(b4)
+plot(b8)
+
+par(mfrow=c(2,2))
 plot(b2, col=clch)
 plot(b3, col=clch)
 plot(b4, col=clch)
@@ -53,10 +59,46 @@ plot(b8, col=clch)
 
 # Let's make a satellite image
 stacksent <- c(b2, b3, b4, b8)
-plot(stacksent, col=clch)
+plot(stacksent) #, col=clch)
 
 dev.off()
 plot(stacksent[[4]], col=clch)
 
+# RGB plotting
+# stacksent[[1]] = b2 = blue
+# stacksent[[2]] = b3 = green
+# stacksent[[3]] = b4 = red
+# stacksent[[4]] = b8 = nir
 
+# plot RGB
+# im.plotRGB(stacksent, r=3, g=2, b=1)
+im.plotRGB(stacksent, 3, 2, 1)
+im.plotRGB(stacksent, 4, 2, 1)
 
+# multiframe
+par(mfrow=c(1,2))
+im.plotRGB(stacksent, 3, 2, 1)
+im.plotRGB(stacksent, 4, 2, 1)
+
+par(mfrow=c(1,3))
+im.plotRGB(stacksent, 3, 2, 1)
+im.plotRGB(stacksent, 4, 2, 1)
+im.plotRGB(stacksent, 4, 3, 2)
+
+dev.off()
+
+# nir on green
+im.plotRGB(stacksent, 3, 4, 2)
+
+# nir on blue
+im.plotRGB(stacksent, 3, 2, 4)
+
+# final multiframe
+par(mfrow=c(2,2))
+im.plotRGB(stacksent, 3, 2, 1)
+im.plotRGB(stacksent, 4, 2, 1)
+im.plotRGB(stacksent, 3, 4, 2)
+im.plotRGB(stacksent, 3, 2, 4)
+
+# correlation of information
+pairs(stacksent)

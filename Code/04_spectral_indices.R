7061aae53fd77089073c13105eec521819fc86dc
Duccio Rocchini <ducciorocchini@gmail.com>
2024-03-25 10:49:37 +0100

    Update 04_spectral_indices.R

--- Code/04_spectral_indices.R
+++ Code/04_spectral_indices.R
@@ -29,6 +29,14 @@ im.plotRGB(m2006, 2, 1, 3)
 # nir on the blue component of RGB
 im.plotRGB(m2006, 2, 3, 1)
 
+# multiframe
+par(mfrow=c(2,3))
+im.plotRGB(m1992, 1, 2, 3) # nir on R 1992
+im.plotRGB(m1992, 2, 1, 3) # nir on G 1992
+im.plotRGB(m1992, 2, 3, 1) # nir on B 1992
+im.plotRGB(m2006, 1, 2, 3) # nir on R 2006
+im.plotRGB(m2006, 2, 1, 3) # nir on G 2006
+im.plotRGB(m2006, 2, 3, 1) # nir on B 2006
 
 
 

c0df31e2887b910831ca478e081aa044c8f743dc
Duccio Rocchini <ducciorocchini@gmail.com>
2024-03-25 09:41:56 +0100

    Update 04_spectral_indices_temp.R

--- Code/04_spectral_indices_temp.R
+++ Code/04_spectral_indices_temp.R
@@ -1,92 +1,61 @@
-library(raster)
+# vegetation indices
 
-setwd("~/lab/") # Linux
-# setwd("C:/lab/") # Windows
-# setwd("/Users/name/Desktop/lab/") # Mac
+library(imageRy)
+library(terra)
 
-# Exercise: import the first file -> defor1_.jpg -> give it the name l1992
-l1992 <- brick("defor1_.png")
+im.list()
 
-plotRGB(l1992, r=1, g=2, b=3, stretch="lin")
+m1992 <- im.import("matogrosso_l5_1992219_lrg.jpg")   
+# bands: 1=NIR, 2=RED, 3=GREEN
+im.plotRGB(m1992, r=1, g=2, b=3)
+im.plotRGB(m1992, 1, 2, 3)
+im.plotRGB(m1992, r=2, g=1, b=3)
+im.plotRGB(m1992, r=2, g=3, b=1)
 
-# layer 1 = NIR
-# layer 2 = red
-# layer 3 = green
+# import the recent image
+m2006 <- im.import("matogrosso_ast_2006209_lrg.jpg")
+im.plotRGB(m2006, r=2, g=3, b=1)
 
+# build a multiframe with 1992 and 2006 images
+par(mfrow=c(1,2))
+im.plotRGB(m1992, r=2, g=3, b=1)
+im.plotRGB(m2006, r=2, g=3, b=1)
 
-# Exercise: import the second file -> defor2_.jpg -> give it the name l2006
-l2006 <- brick("defor2_.png")
+# DVI = NIR - RED
+# bands: 1=NIR, 2=RED, 3=GREEN
 
-# l2006 <- brick("p224r63_2011.grd")
-l2006
+dvi1992 = m1992[[1]] - m1992[[2]]
+plot(dvi1992)
 
-plotRGB(l2006, r=1, g=2, b=3, stretch="lin")
-
-# Exercise: plot in a multiframe the two images with one on top of the other
-par(mfrow=c(2,1))
-plotRGB(l1992, r=1, g=2, b=3, stretch="lin")
-plotRGB(l2006, r=1, g=2, b=3, stretch="lin")
-
-# DVI Difference Vegetation Index
-dvi1992 = l1992[[1]] - l1992[[2]]
-# or:
-# dvi1992 = l1992$defor1_.1 - l1992$defor1_.2
-dvi1992
-
-cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) # specifying a color scheme
+cl <- colorRampPalette(c("darkblue", "yellow", "red", "black")) (100)
 plot(dvi1992, col=cl)
 
-# DVI Difference Vegetation Index
-dvi2006 = l2006[[1]] - l2006[[2]]
-dvi2006
-
+# exercise: calculate dvi of 2006
+dvi2006 = m2006[[1]] - m2006[[2]]
 plot(dvi2006, col=cl)
 
-
-# DVI difference in time
-dvi_dif = dvi1992 - dvi2006
-cld <- colorRampPalette(c('blue','white','red'))(100)
-plot(dvi_dif, col=cld)
-
-#- day 2
-
-# Range DVI (8 bit): -255 a 255
-# Range NDVI (8 bit): -1 a 1
-
-# Range DVI (16 bit): -65535 a 65535
-# Range NDVI (16 bit): -1 a 1
-
-# Hence, NDVI can be used to compare images with a different radiometric resolution
-
-# NDVI 1992
-dvi1992 = l1992[[1]] - l1992[[2]]
-ndvi1992 = dvi1992 / (l1992[[1]] + l1992[[2]])
-# or
-ndvi1992 = (l1992[[1]] - l1992[[2]]) / (l1992[[1]] + l1992[[2]])
-
-cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) # specifying a color scheme
+# NDVI
+ndvi1992 = (m1992[[1]] - m1992[[2]]) / (m1992[[1]] + m1992[[2]])
+ndvi1992 = dvi1992 / (m1992[[1]] + m1992[[2]])
 plot(ndvi1992, col=cl)
 
-# multiframe con il plotRGB dell'immagine sopra
-# e l'ndvi sotto
+# NDVI
+ndvi2006 = dvi2006 / (m2006[[1]] + m2006[[2]])
+plot(ndvi2006, col=cl)
 
-# Multiframe with plotRGB on top of the NDVI image
-par(mfrow=c(2,1))
-plotRGB(l1992, r=1, g=2, b=3, stretch="lin")
-plot(ndvi1992, col=cl)
-
-# 2006
-ndvi2006 = dvi2006 / (l2006[[1]] + l2006[[2]])
-
-# Multiframe with NDVI1992 on top of the NDVI2006 image
-par(mfrow=c(2,1))
+# par
+par(mfrow=c(1,2))
 plot(ndvi1992, col=cl)
 plot(ndvi2006, col=cl)
 
+clvir <- colorRampPalette(c("violet", "dark blue", "blue", "green", "yellow"))(100) # specifying a color scheme
+par(mfrow=c(1,2))
+plot(ndvi1992, col=clvir)
+plot(ndvi2006, col=clvir)
 
-
-
-
+# speediing up calculation
+ndvi2006a <- im.ndvi(m2006, 1, 2)
+plot(ndvi2006a, col=cl)
 
 
 

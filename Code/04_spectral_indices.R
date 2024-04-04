7e03aec8fa6fbd30ecece0ef4dd9eea700ffd539
Duccio Rocchini <ducciorocchini@gmail.com>
2024-04-04 17:35:19 +0200

    Update 04_spectral_indices.R

--- Code/04_spectral_indices.R
+++ Code/04_spectral_indices.R
@@ -38,9 +38,40 @@ im.plotRGB(m2006, 1, 2, 3) # nir on R 2006
 im.plotRGB(m2006, 2, 1, 3) # nir on G 2006
 im.plotRGB(m2006, 2, 3, 1) # nir on B 2006
 
+#---
 
+# Calculating the DVI (Difference Vegetation Index)
+dvi1992 = m1992[[1]] - m1992[[2]] 
+# alternative way of coding:
+# dvi1992 = m1992$matogrosso~2219_lrg_1 - m1992$matogrosso~2219_lrg_2
 
+# plotting the DVI
+cl <- colorRampPalette(c("darkblue", "yellow", "red", "black")) (100)
+plot(dvi1992, col=cl)
 
+# 2006
+m2006 <- im.import("matogrosso_ast_2006209_lrg.jpg")
+
+# dvi 2006
+dvi2006 = m2006[[1]] - m2006[[2]] 
+plot(dvi2006, col=cl)
+
+# Exercise: plot the dvi1992 beside the dvi2006
+par(mfrow=c(1,2))
+plot(dvi1992, col=cl)
+plot(dvi2006, col=cl)
+
+stackdvi <- c(dvi1992, dvi2006)
+pairs(stackdvi)
+
+# Normalized Difference Vegetation Index
+ndvi1992 = dvi1992 / (m1992[[1]]+m1992[[2]])
+ndvi2006 = dvi2006 / (m2006[[1]]+m2006[[2]])
+
+dev.off()
+par(mfrow=c(1,2))
+plot(ndvi1992, col=cl)
+plot(ndvi2006, col=cl)
 
 
 
